e3b5bc32aec5bac3947d791fb97f0efe78ae59aa
Veer-Saraswat <161647605+Veer-Saraswat@users.noreply.github.com>
2024-03-20 16:46:01 -0400

    Add files via upload
    version 2.0

--- dectree.py
+++ dectree.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import Float32
+from std_msgs.msg import String
 
 class DecisionTree:
     def __init__(self):
@@ -12,42 +12,37 @@ class DecisionTree:
         self.temperature = 0
         self.wind_speed = 0
 
-        # Defining Subscribers
-        rospy.Subscriber('soc', Float32, self.soc_callback)
-        rospy.Subscriber('snow', Float32, self.snow_callback)
-        rospy.Subscriber('temperature', Float32, self.temperature_callback)
-        rospy.Subscriber('wind_speed', Float32, self.wind_speed_callback)
+        # Defining Subscriber for weather forecast
+        rospy.Subscriber('weather_forecast', String, self.weather_forecast_callback)
 
         self.rate = rospy.Rate(1 / 1800.0)  # scanning every 30 minutes
 
-    def soc_callback(self, data):
-        self.soc = data.data
-
-    def snow_callback(self, data):
-        snow_value = data.data
-        self.snow_counter += snow_value # increment snow counter every time snow topic is updated 
-
-    def temperature_callback(self, data):
-        self.temperature = data.data
-
-    def wind_speed_callback(self, data):
-        self.wind_speed = data.data
+    def weather_forecast_callback(self, data):
+        weather_data = data.data.split(', ')
+        for item in weather_data:
+            key, value = item.split(': ')
+            if key == 'Snow Level':
+                self.snow_counter += float(value.replace(' mm', ''))
+            elif key == 'Temperature':
+                self.temperature = float(value.replace(' °C', ''))
+            elif key == 'Wind Speed':
+                self.wind_speed = float(value.replace(' kph', ''))
 
     def run_decision_tree(self):
         while not rospy.is_shutdown():
-            #if self.soc < 60:
-                #rospy.loginfo("Staying in (SOC < 60)")
+            if self.soc < 60:
+                rospy.loginfo("Staying in (SOC < 60)")
                 if self.snow_counter > 3:
                     rospy.loginfo("Staying in (Snow counter > 3mm)")
                     if self.temperature > 1:
                         rospy.loginfo("Staying in (Temperature > 1°C)")
                         if self.wind_speed > 3:
                             rospy.loginfo("Staying in (Wind Speed > 3 km/h)")
-                else:
-                    rospy.loginfo("Clearing driveway")
-                    self.snow_counter = 0  # Reset snow counter
+            else:
+                rospy.loginfo("Clearing driveway")
+                self.snow_counter = 0  # Reset snow counter
 
-                self.rate.sleep()
+            self.rate.sleep()
 
 if __name__ == '__main__':
     try:
