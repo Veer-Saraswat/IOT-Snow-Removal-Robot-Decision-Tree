86e7b9c6f060dfa023d140c33825ad8ea6d2a9b8
Veer-Saraswat <161647605+Veer-Saraswat@users.noreply.github.com>
2024-04-25 15:07:02 -0400

    Update decision_tree.py
    updated with correct battery monitoring, and allowing autonomous navigation launch

--- decision_tree.py
+++ decision_tree.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import String, Int32
 
 class DecisionTree:
     def __init__(self):
         rospy.init_node('decision_tree', anonymous=True)
-
-        self.soc = 100  # short hand for State of Charge
+        self.soc = 0  # short hand for State of Charge
         self.snow_counter = 0
         self.temperature = 0
-        #self.wind_speed = 0
-
-        # Defining Subscriber for weather forecast
-        rospy.Subscriber('weather_forecast', String, self.weather_forecast_callback)
-
         self.rate = rospy.Rate(1 / 1800.0)  # scanning every 30 minutes
+
+        # Subscriber for weather forecast
+        rospy.Subscriber('weather_forecast', String, self.weather_forecast_callback)
+        # Subscriber for battery percentage
+        rospy.Subscriber('/robot/battery', Int32, self.battery_percentage_callback)
 
     def weather_forecast_callback(self, data):
         weather_data = data.data.split(', ')
@@ -25,12 +24,17 @@ class DecisionTree:
                 self.snow_counter += float(value.replace(' mm', ''))
             elif key == 'Temperature':
                 self.temperature = float(value.replace(' °C', ''))
-            elif key == 'Wind Speed':
-                self.wind_speed = float(value.replace(' kph', ''))
+
+    def battery_percentage_callback(self, data):
+        # Update soc from the battery percentage received
+        try:
+            self.soc = data.data
+        except ValueError:
+            rospy.logerr("Invalid battery percentage received")
 
     def run_decision_tree(self):
         while not rospy.is_shutdown():
-            if self.soc >= 60 and self.temperature <= 1 and self.snow_counter <= 3: #self.wind_speed <= 3
+            if self.soc >= 60 and self.temperature <= 1 and self.snow_counter <= 3:
                 rospy.loginfo("Clearing driveway")
                 self.snow_counter = 0  # Reset snow counter
             else:
